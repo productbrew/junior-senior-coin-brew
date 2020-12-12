7e04673b385318630eb1c47a362fb5528eda28c9
czystyl <lukasz.czyszczonik@gmail.com>
2020-12-12 18:45:43 +0100

    Add exchange cache

--- apps/coin-brew/src/main.tsx
+++ apps/coin-brew/src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/app';
-import { createClient, Provider } from 'urql';
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql';
+import { cacheExchange } from '@urql/exchange-graphcache';
 
 import 'antd/dist/antd.css';
 
 const client = createClient({
   url: 'http://localhost:3000/graphql',
+  exchanges: [dedupExchange, cacheExchange(), fetchExchange],
 });
 
 ReactDOM.render(
